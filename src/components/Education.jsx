b7ae74ce0733380e3fea6ea70c44590afcb819f1
Naved khan <nomanali50997@gmail.com>
2025-01-29 14:25:31 +0530

    debugging

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -11,29 +11,28 @@ const Education = () => {
 
     const divRef = useRef()
 
-   useGSAP(()=>{
-    gsap.from(divRef.current,{
-        y: 100,
-        opacity: 0, 
-        delay: 0.5,
-        duration: 1,
-        scrollTrigger:{
-            scroller: "body",
-            trigger: divRef.current,
-            start: "top 50%",
-            end: "bottom 30%",
-            markers:true,
-        }
+    useGSAP(() => {
+        gsap.from(divRef.current, {
+            y: 100,
+            opacity: 0,
+            delay: 0.5,
+            duration: 1,
+            scrollTrigger: {
+                scroller: "body",
+                trigger: divRef.current,
+                start: "top 50%",
+                end: "bottom 30%",
+            }
+        })
     })
-   })
     return (
         <div className="bg-zinc-900">
             <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8" >
                 <div className="w-full  h-1 bg-[#FF7051] mx-auto"></div>
                 <h1 className="text-2xl sm:text-6xl text-[#FF7051] font-bold mt-4">Education</h1>
-                <div  className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center mt-4">
-                    <div ref={divRef}  className="flex flex-col items-start gap-x-4 gap-y-8 mt-4 mb-8">
-                        <div  className=" bg-[#f565394d] hover:bg-[#FF7051] transition duration-500 ease-in-out rounded-3xl  h-[110px] w-[90%] md:w-[100%] group ">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center mt-4">
+                    <div ref={divRef} className="flex flex-col items-start gap-x-4 gap-y-8 mt-4 mb-8">
+                        <div className="bg-[#f565394d] hover:bg-[#FF7051] transition duration-500 ease-in-out rounded-3xl  h-[110px] w-[90%] md:w-[100%] group ">
                             <div className="grid grid-cols-[1fr_3fr] items-center overflow-hidden ">
                                 <div className="mt-4 ml-4 bg-[#dc654a] rounded-full w-12 h-12 sm:w-16 sm:h-16 ">
                                     <img className="w-16 h-16" src={assets.graduationIcon} alt="" />
@@ -45,7 +44,7 @@ const Education = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="two bg-[#f565394d] hover:bg-[#FF7051] transition duration-500 ease-in-out  rounded-3xl  h-[110px] w-[90%] md:w-[100%] group">
+                        <div className="bg-[#f565394d] hover:bg-[#FF7051] transition duration-500 ease-in-out  rounded-3xl  h-[110px] w-[90%] md:w-[100%] group">
                             <div className="grid grid-cols-[1fr_3fr] items-center overflow-hidden ">
                                 <div className="mt-4 ml-4 bg-[#dc654a] rounded-full w-12 h-12 sm:w-16 sm:h-16">
                                     <img className="w-16 h-16" src={assets.graduationIcon} alt="" />
@@ -57,7 +56,7 @@ const Education = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="three bg-[#f565394d] hover:bg-[#FF7051] transition duration-500 ease-in-out  rounded-3xl  h-[110px] w-[90%] md:w-[100%] group">
+                        <div className="bg-[#f565394d] hover:bg-[#FF7051] transition duration-500 ease-in-out  rounded-3xl  h-[110px] w-[90%] md:w-[100%] group">
                             <div className="grid grid-cols-[1fr_3fr] items-center overflow-hidden ">
                                 <div className="mt-4 ml-4 bg-[#dc654a] rounded-full w-12 h-12 sm:w-16 sm:h-16">
                                     <img className="w-16 h-16" src={assets.graduationIcon} alt="" />
@@ -69,7 +68,7 @@ const Education = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="four bg-[#f565394d] hover:bg-[#FF7051] transition duration-500 ease-in-out  rounded-3xl  h-[128px] w-[90%] md:w-[100%]  group">
+                        <div className="bg-[#f565394d] hover:bg-[#FF7051] transition duration-500 ease-in-out  rounded-3xl  h-[128px] w-[90%] md:w-[100%]  group">
                             <div className="grid grid-cols-[1fr_3fr] items-center overflow-hidden ">
                                 <div className="mt-4 ml-4 bg-[#dc654a] rounded-full w-12 h-12 sm:w-16 sm:h-16 ">
                                     <img className="w-16 h-16" src={assets.graduationIcon} alt="" />
