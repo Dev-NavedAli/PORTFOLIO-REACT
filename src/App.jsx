53d1a0acaad756be94f3e13a3cc0ccc2e6657609
Naved khan <nomanali50997@gmail.com>
2025-02-03 10:13:56 +0530

    configuring the backend for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import About from './components/About'
-// import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Skills from './components/Skills'
 import Projects from './components/Project'
